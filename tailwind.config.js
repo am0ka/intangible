174b9dbe190e84a688e2314ba779d92d6e56bf69
Amir Sarsenov <me@amirsarsenov.dev>
2025-05-17 22:02:22 +0200

    Initial commit: Add project configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          300: '#d1d5db',
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+}
